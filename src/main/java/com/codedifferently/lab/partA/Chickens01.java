ecfc20f94bfb0ed106aedd11a589e8a221d2ac24
KW316 <kiannamarie.wilson@cdpipelinedevshop.com>
2025-01-02 09:55:05 -0500

    Finished Project Supposedly?

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -1,7 +1,46 @@
 package com.codedifferently.lab.partA;
-
+import java.util.Scanner;
 public class Chickens01 {
     //Put your code here
+    public static void main(String[]args) {
+        int totaleggs = 0;
+         int chickens = 0;
+        int day = 0;
+        int x = 0;
+        System.out.println("How many Chickens do you start with?");
+        Scanner keyboard = new Scanner(System.in);
+        chickens = keyboard.nextInt();
+        while(day < 3) {
+            if( chickens < 4){
+                x = 5;
+            if(day == 0){
+                totaleggs= totaleggs + (chickens * x);
+            }else if( day == 1){
+                chickens++;
+                totaleggs= totaleggs + (chickens * x);
+            }else{
+                chickens = chickens/2;
+                totaleggs= totaleggs + (chickens * x);
+            }
+            day++;
+            }else{
+              x = 4;
+                if(day == 0){
+                    totaleggs= totaleggs + (chickens * x);
+                }else if( day == 1){
+                    chickens++;
+                    totaleggs= totaleggs + (chickens * x);
+                }else{
+                    chickens = chickens/2;
+                    totaleggs= totaleggs + (chickens * x);
+                }
+                day++;
+            }
+        }
+        System.out.println(totaleggs);
+    }
 
-    //System.out.println(totalEggs);
+
 }
+
+
--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -3,9 +3,14 @@ package com.codedifferently.lab.partA;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
-        //System.out.println("Daily Average:   " +dailyAverage);
-        //System.out.println("Monthly Average: " +monthlyAverage);
-        //System.out.println("Monthly Profit:  $" +monthlyProfit);
+double dailyAverage = 0;
+double monthlyAverage = 0;
+double monthlyProfit = 0;
+dailyAverage = (double) (100 + 121 + 117) /3;
+monthlyAverage = (double) (100 + 121 + 117) /12; // I chose 12 because generally in a 30 day time period there a at least 4 weeks so i assumes there would be 4 sets of MOn.Tues, Wednes
+monthlyProfit = monthlyAverage * .18;
+        System.out.println("Daily Average:   " +dailyAverage);
+        System.out.println("Monthly Average: " +monthlyAverage);
+        System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
 }
